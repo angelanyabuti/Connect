15295f34f03a61caa39f4645136cbdc4a046cfb5
Angela Nyabuti <nyabutiangella@gmail.com>
2021-12-14 14:32:13 +0300

    Updated the employee page

---
+++ EmployeeDetails.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Connect.Master" AutoEventWireup="true" CodeBehind="EmployeeDetails.aspx.cs" Inherits="Connect.EmployeeDetails" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+<p>
+    &nbsp;</p>
+    <p>
+        &nbsp;</p>
+    <asp:Label ID="Label7" runat="server" Text="Employee ID"></asp:Label>
+<asp:TextBox ID="txtEid" runat="server"></asp:TextBox>
+    <p>
+    <asp:Label ID="Label1" runat="server" Text="Full names"></asp:Label>
+    <asp:TextBox ID="txtFnames" runat="server"></asp:TextBox>
+</p>
+<p>
+    <asp:Label ID="Label2" runat="server" Text="Email"></asp:Label>
+    <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+</p>
+<p>
+    <asp:Label ID="Label3" runat="server" Text="Mobile number"></asp:Label>
+    <asp:TextBox ID="txtEnumber" runat="server"></asp:TextBox>
+</p>
+<p>
+    <asp:Label ID="Label4" runat="server" Text="Address"></asp:Label>
+    <asp:TextBox ID="txtAddress" runat="server"></asp:TextBox>
+</p>
+<p>
+    <asp:Label ID="Label5" runat="server" Text="Date of Join"></asp:Label>
+    <asp:TextBox ID="txtDjoin" runat="server"></asp:TextBox>
+</p>
+<p>
+    <asp:Label ID="Label6" runat="server" Text="Department"></asp:Label>
+    <asp:TextBox ID="txtDep" runat="server"></asp:TextBox>
+</p>
+    <p>
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Add" />
+    <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Update" />
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" onClientClick="return confirm('are you sure you want to delete?');" Text="Delete" />
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Search" />
+</p>
+    <p>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+</p>
+
+<p>
+    &nbsp;</p>
+<p>
+    &nbsp;</p>
+</asp:Content>
---
+++ EmployeeDetails.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Connect
+{
+    public partial class EmployeeDetails : System.Web.UI.Page
+    {
+        SqlCommand cmd;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                LoadRecord();
+            }
+        }
+        SqlConnection con= new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Angela\OneDrive\Desktop\Connect\Connect\App_Data\Connect.mdf;Integrated Security=True");
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+           
+            //writing sql query
+
+            SqlCommand cmd = new SqlCommand("insert into Employee (EmpId, Full_names, Email, Mobile_no, Address, Date_of_Join,Department) values('" + txtEid.Text + "','" + txtFnames.Text + "', '" + txtEmail.Text + "', '" + txtEnumber.Text + "','" + txtAddress.Text + "' ,'"+ txtDjoin.Text + "','" + txtDep.Text + "')", con);
+
+            // Opening Connection
+            con.Open();
+            //Executing the SQL query
+            cmd.ExecuteNonQuery();
+             con.Close();
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully inserted');", true);
+            }
+            // Closing the connection
+            
+            LoadRecord();
+        }
+        void LoadRecord()
+        {
+            SqlCommand cmd = new SqlCommand("select * from Employee", con);
+            SqlDataAdapter d = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            d.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            //writing sql query
+
+            SqlCommand cmd = new SqlCommand("update Employee (EmpId, Full_names, Email, Mobile_no, Address, Date_of_Join,Department) set Full_names = '" + txtFnames.Text + "',Email = '" + txtEmail.Text + "',Mobile_no = '" + txtEnumber.Text + "', Address = '" + txtAddress.Text + "', Date_of_Join= '" + txtDjoin.Text + "',Department = '" + txtDep.Text + "' where EmpID = '" + txtEid + "'", con);
+
+            // Opening Connection
+            con.Open();
+            //Executing the SQL query
+            cmd.ExecuteNonQuery( );
+            con.Close();
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully updated');", true);
+            }
+            // Closing the connection
+
+            LoadRecord();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            //writing sql query
+
+            SqlCommand cmd = new SqlCommand("Delete Employee (EmpId, Full_names, Email, Mobile_no, Address, Date_of_Join,Department)  where EmpID = '" + txtEid + "'", con);
+
+            // Opening Connection
+            con.Open();
+            //Executing the SQL query
+                        cmd.ExecuteNonQuery( );
+
+            con.Close();
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully deleted');", true);
+            }
+            // Closing the connection
+
+            LoadRecord();
+        }
+        //search button
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+
+            SqlCommand cmd = new SqlCommand("select * from Employee where EmpID = '" + int.Parse(txtEid.Text) + "'", con);
+            SqlDataAdapter d = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            d.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+        
+    }
+}
